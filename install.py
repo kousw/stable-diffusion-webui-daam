1de736ecf204495328dd84fb3e557b507c0609e5
Bingsu <ks2515@naver.com>
2023-01-21 22:33:26 +0900

    check matplotlib version

--- install.py
+++ install.py
@@ -1,3 +1,20 @@
 import launch
-if not launch.is_installed('matplotlib'):
-    launch.run_pip('install matplotlib==3.6.2', desc='Installing matplotlib==3.6.2')
+
+
+def check_matplotlib():
+    if not launch.is_installed("matplotlib"):
+        return False
+
+    try:
+        import matplotlib
+    except ImportError:
+        return False
+
+    if hasattr(matplotlib, "__version_info__"):
+        version = matplotlib.__version_info__
+        return version.major >= 3 and version.minor >= 6 and version.micro >= 2
+    return False
+
+
+if not check_matplotlib():
+    launch.run_pip("install matplotlib==3.6.2", desc="Installing matplotlib==3.6.2")
